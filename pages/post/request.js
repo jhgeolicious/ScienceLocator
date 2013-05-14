40290e64ffb5cb3f900ae48c2e1ff8a953aeb7c6
Danijar Hafner <pfisrik@gmail.com>
2013-05-15 01:42:56 +0200

    implemented polygon drawing

--- pages/post/request.js
+++ pages/post/request.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 
+	/****************************************************************
+	 * map initlialization                                          *
+	 ****************************************************************/
 	var map = L.map('map', {
 		minZoom   : 0,
 		maxZoom   : 11,
@@ -9,9 +12,77 @@ $(document).ready(function(){
 
 	var terrain  = L.tileLayer('http://oatile1.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.jpg').addTo(map);
 
+	/****************************************************************
+	 * debug print                                                  *
+	 ****************************************************************/
 	map.on('mousemove', function(e){
 		var lng = L.Util.formatNum(e.latlng.lng, 5);
 		var lat = L.Util.formatNum(e.latlng.lat, 5);
 		$('#debug').html('Lng ' + lng + ' Lat ' + lat);
 	}, this);
+
+	/****************************************************************
+	 * polygon drawing                                              *
+	 ****************************************************************/
+	var drawing = false, polygon = L.polygon([]).addTo(map), points = [];
+
+	
+	$('#area input[type="button"]').click(function(){
+		drawing_state();
+	});
+
+	// start and stop setting points
+	map.on('click', function(e){
+		if(drawing)
+		{
+			points.push(e.latlng);
+			update_polygon(points);
+		}
+	});
+
+	$(document).keyup(function(e) {
+		if(e.keyCode == 27)
+			drawing_state(false);
+	});
+
+	// preview next point
+	map.on('mousemove', function(e){
+		if(drawing)
+			update_polygon(points.concat(e.latlng));
+	}, this);
+
+	// helper functions
+	var area_button_label = $('#area input[type="button"]').val();
+	function drawing_state(state)
+	{
+		if(typeof state === 'undefined')
+			state = !drawing;
+
+		if(state)
+		{
+			points = [];
+			$('#area input[type="button"]').val('I am ready');
+		}
+		else
+		{
+			update_polygon(points);
+			$('#area input[type="button"]').val(area_button_label);
+		}
+
+		drawing = state;
+	}
+
+	function update_polygon(points)
+	{
+		map.removeLayer(polygon);
+		polygon = L.polygon(points, {
+			color:      '#f60',
+			opacity:     0.8,
+			weight:      7,
+			fillColor:  '#f60',
+			fillOpacity: 0.3,
+		});
+		polygon.addTo(map);
+	}
+	
 });
--- pages/search/request.js
+++ pages/search/request.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 
+	/****************************************************************
+	 * map initlialization                                          *
+	 ****************************************************************/
 	var map = L.map('map', {
 		minZoom   : 0,
 		maxZoom   : 11,
@@ -8,32 +11,99 @@ $(document).ready(function(){
 	}).setView([20, -20], 0);
 
 	var terrain  = L.tileLayer('http://oatile1.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.jpg').addTo(map);
-	
-	var polygons = L.geoJson(null, {
-		onEachFeature: function(feature, layer){
-			debug = feature.geometry.coordinates;
-			layer.bindPopup(feature.properties.title + '<br><span style="color:#aaa;">' + 'Debug: ' + debug + "</span>");
-		},
-	}).addTo(map);
 
+	/****************************************************************
+	 * debug print                                                  *
+	 ****************************************************************/
 	map.on('mousemove', function(e){
 		var lng = L.Util.formatNum(e.latlng.lng, 5);
 		var lat = L.Util.formatNum(e.latlng.lat, 5);
 		$('#debug').html('Lng ' + lng + ' Lat ' + lat);
 	}, this);
 
-	function search_request(title)
+	/****************************************************************
+	 * polygon drawing                                              *
+	 ****************************************************************/
+	var drawing = false, polygon = L.polygon([]).addTo(map), points = [];
+
+	
+	$('#area input[type="button"]').click(function(){
+		drawing_state();
+	});
+
+	// start and stop setting points
+	map.on('click', function(e){
+		if(drawing)
+		{
+			points.push(e.latlng);
+			update_polygon(points);
+		}
+	});
+	
+	$(document).keyup(function(e) {
+		if(e.keyCode == 27)
+			drawing_state(false);
+	});
+
+	// preview next point
+	map.on('mousemove', function(e){
+		if(drawing)
+			update_polygon(points.concat(e.latlng));
+	}, this);
+
+	// helper functions
+	var area_button_label = $('#area input[type="button"]').val();
+	function drawing_state(state)
+	{
+		if(typeof state === 'undefined')
+			state = !drawing;
+
+		if(state)
+		{
+			polygons.clearLayers();
+			points = [];
+			$('#area input[type="button"]').val('I am ready');
+		}
+		else
+		{
+			update_polygon(points);
+			$('#area input[type="button"]').val(area_button_label);
+		}
+
+		drawing = state;
+	}
+
+	function update_polygon(points)
+	{
+		map.removeLayer(polygon);
+		polygon = L.polygon(points, {
+			color:      '#f60',
+			opacity:     0.8,
+			weight:      7,
+			fillColor:  '#f60',
+			fillOpacity: 0.3,
+		});
+		polygon.addTo(map);
+	}
+
+	/****************************************************************
+	 * search request                                               *
+	 ****************************************************************/
+
+	function search_request(title, points)
 	{
 		var request = $.ajax({
 			url        : 'pages/search/request.php',
 			type       : 'POST',
 			dataType   : 'json',
 			data       : {
-							title : title || '',
+							title  : title  || '',
+							points : points || '',
 						 },
 			cache      : false,
 			beforeSend : function(){
 				$('#results').html('<li><h3>Wait for it...</h3></li>');
+				map.removeLayer(polygon);
 				polygons.clearLayers();
 			},
 			success : function(json){
@@ -48,6 +118,21 @@ $(document).ready(function(){
 		});
 	}
 
+	$('#keywords input[type="button"]').click(function(){
+		search_request($('#keywords input[type="text"]').val());
+	});
+
+	/****************************************************************
+	 * display results                                              *
+	 ****************************************************************/
+
+	var polygons = L.geoJson(null, {
+		onEachFeature: function(feature, layer){
+			debug = feature.geometry.coordinates;
+			layer.bindPopup(feature.properties.title + '<br><span style="color:#aaa;">' + 'Debug: ' + debug + "</span>");
+		},
+	}).addTo(map);
+
 	function list_result(properties)
 	{
 		$('#results').append('<li tag="' + (properties.id          || ''              ) + '">'
@@ -57,8 +142,4 @@ $(document).ready(function(){
 		                   + '<p>'       + (properties.description || 'no description') + '</p>'
 		                   + '</li>');
 	}
-
-	$('#keywords input[type="button"]').click(function(){
-		search_request($('#keywords input[type="text"]').val());
-	});
 });
