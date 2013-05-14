dd138ac3c62ecd19fd652314bca89fc30c7f588c
Danijar Hafner <pfisrik@gmail.com>
2013-05-14 18:03:34 +0200

    Got map bounds partly working

--- pages/search/script.js
+++ pages/search/script.js
@@ -3,9 +3,9 @@ $(document).ready(function(){
 	var map = L.map('map', {
 		minZoom   : 0,
 		maxZoom   : 11,
-		maxBounds : [[90, -180], [-90, 180]],
+		maxBounds : [[-75, -180], [75, 180]],
 		crs       : L.CRS.EPSG3857,
-	}).setView([0, 0], 0);
+	}).setView([20, -20], 0);
 
 	var terrain  = L.tileLayer('http://oatile1.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.jpg').addTo(map);
 	
